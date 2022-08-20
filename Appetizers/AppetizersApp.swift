64a4f61e1378b976af6e7c62b9b6a507a4f6f427
Martin Stofko <martinstofko114@gmail.com>
2022-08-19 22:42:14 -0700

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Martin Stofko on 8/19/22.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
