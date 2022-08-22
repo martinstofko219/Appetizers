01633ff15441a8c4c2a518c5013d921c1dde28ac
Martin Stofko <martinstofko114@gmail.com>
2022-08-21 19:46:01 -0700

    Network manager and custom app error.

---
+++ Appetizers/AppError.swift
@@ -0,0 +1,15 @@
+//
+//  AppError.swift
+//  Appetizers
+//
+//  Created by Martin Stofko on 8/21/22.
+//
+
+import Foundation
+
+enum AppError: Error {
+    case invalidURL
+    case invalidResponse
+    case invalidData
+    case unableToComplete
+}
